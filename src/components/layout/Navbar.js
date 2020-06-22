e036c999b318fac0170c6e00accaf0bd6f3c80db
DESKTOP-0VSV11Q\sag_h <sag_h13@hotmail.com>
2020-06-23 00:10:23 +0500

    fix modile nav

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,20 @@
 import React, { Fragment, useContext } from 'react'
 import { Link } from 'react-router-dom'
+
+import M from 'materialize-css/dist/js/materialize.min.js'
+
 import AuthContext from '../../context/auth/authContext'
+import { useEffect } from 'react'
 
 const Navbar = () => {
   const authContext = useContext(AuthContext)
 
   const { logout, isAuthenticated } = authContext
+
+  useEffect(() => {
+    let sidenav = document.querySelector('#nav-mobile')
+    M.Sidenav.init(sidenav, {})
+  }, [])
 
   const onLogout = () => {
     logout()
@@ -30,16 +39,30 @@ const Navbar = () => {
   )
 
   return (
-    <nav>
-      <div className='nav-wrapper blue-grey darken-3'>
-        <a href='/' className='brand-logo'>
-          Task for Emphasoft
-        </a>
-        <ul id='nav-mobile' className='right hide-on-med-and-down'>
-          {isAuthenticated ? authLinks : guestLinks}
-        </ul>
-      </div>
-    </nav>
+    <Fragment>
+      <nav className='nav-extended'>
+        <div className='nav-wrapper blue-grey darken-3'>
+          <a href='/' className='brand-logo'>
+            For{'\u00A0'}Emphasoft
+          </a>
+          <a
+            href='#!'
+            data-target='nav-mobile'
+            className='sidenav-trigger'
+            style={{ margin: '0 0' }}
+          >
+            <i className='material-icons'>menu</i>
+          </a>
+          <ul className='right hide-on-med-and-down'>
+            {isAuthenticated ? authLinks : guestLinks}
+          </ul>
+        </div>
+      </nav>
+
+      <ul id='nav-mobile' className='sidenav'>
+        {isAuthenticated ? authLinks : guestLinks}
+      </ul>
+    </Fragment>
   )
 }
 export default Navbar
